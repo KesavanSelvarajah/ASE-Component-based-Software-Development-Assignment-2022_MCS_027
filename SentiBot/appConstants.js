416c5811a4dbf09bddc48ddba68ea1271f578901
Kesavan Selvarajah <37371215+KesavanSelvarajah@users.noreply.github.com>
2023-04-28 20:49:29 +0530

    initial commit

---
+++ SentiBot/appConstants.js
@@ -0,0 +1,13 @@
+module.exports = {
+  EVENT_CUSTOMER_CONNECTED: 'customer connected',
+  EVENT_CUSTOMER_DISCONNECTED: 'customer disconnected',
+  EVENT_CUSTOMER_MESSAGE: 'customer message',
+  EVENT_OPERATOR_MESSAGE: 'operator message',
+  EVENT_OPERATOR_REQUESTED: 'operator requested',
+  EVENT_SYSTEM_ERROR: 'system error',
+  EVENT_DISCONNECT: 'disconnect',
+
+  CONTEXT_OPERATOR_REQUEST: 'operator_request',
+
+  OPERATOR_GREETING: `Greetings, I am a human customer care representative. Please accept my sincere apologies for any inconvenience you may have experienced. How may I be of assistance to you?`
+};
